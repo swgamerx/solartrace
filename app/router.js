eed7e981704045f969959488b997cdc4ed14cace
swgamerx <santeau@gmail.com>
2016-11-03 22:26:41 -0500

    Adding comments to the router
    Realized that these routes might not make sense without any context
    existing.

--- app/router.js
+++ app/router.js
@@ -7,10 +7,20 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+
+  // User Registration
   this.route('register');
+
+  // User Login
   this.route('login');
+
+  // List of companies that people have added traces to 
   this.route('companies');
+
+  // View for a specific company and all of it's traces
   this.route('company');
+
+  // Add a trace, includes finding a business location to add the trace to
   this.route('trace');
 });
 
