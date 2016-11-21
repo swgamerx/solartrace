f471d4971a5739650aa606ce2746932f66a4e25a
swgamerx <santeau@gmail.com>
2016-11-20 21:11:15 -0600

    Tracing is now working but not yet saving the trace.

--- app/controllers/trace.js
+++ app/controllers/trace.js
@@ -4,6 +4,11 @@ export default Ember.Controller.extend({
     lat: 36.174465,
     lng: -86.767960,
     zoom: 10,
+    groundPoints: Ember.A([]),
+    roofPoints: Ember.A([]),
+    groundZone: Ember.computed('groundPoints.@each.lat', 'groundPoints.@each.lng', function(){
+        return this.get('groundPoints').map(r => ({lat: r.lat, lng: r.lng}));
+    }),
     actions: {
         findAddress: function(){
             let controller = this;
@@ -13,6 +18,23 @@ export default Ember.Controller.extend({
                 controller.set('lat',data.results[0].geometry.location.lat);
                 controller.set('lng',data.results[0].geometry.location.lng);
                 controller.set('zoom',15);
+                console.log(controller.get('lat'));
+                console.log(controller.get('lng'));
+            });
+        },
+        addPoint: function(e){
+            console.log(e);
+            let location = e.latlng;
+            this.get('groundPoints').pushObject({
+                lat: location.lat,
+                lng: location.lng
+            });
+        },
+        updateLocation(r, e) {
+            let location = e.target.getLatLng();
+            Ember.setProperties(r, {
+                lat: location.lat,
+                lng: location.lng
             });
         }
     }
