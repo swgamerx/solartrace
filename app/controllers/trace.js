b090b39fef08762d4f090e594026f82550af4369
swgamerx <santeau@gmail.com>
2017-01-17 23:50:37 -0600

    updated trace controller to help the template show buttons once an address has been found. Starting the functions to create the new address and the traces applied to it.

--- app/controllers/trace.js
+++ app/controllers/trace.js
@@ -9,19 +9,24 @@ export default Ember.Controller.extend({
     groundZone: Ember.computed('groundPoints.@each.lat', 'groundPoints.@each.lng', function(){
         return this.get('groundPoints').map(r => ({lat: r.lat, lng: r.lng}));
     }),
+    locationSet: false,
+    googleMapAddress: null,
     actions: {
+        // Find the address the user submitted
         findAddress: function(){
-            let controller = this;
+            let controller = this; 
+            console.log('find address');
             let address = this.get('address') + ' ' + this.get('city') + ' ' + this.get('state');
             $.ajax('http://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&sensor=false').done(function(data){
-                console.log(data);
+                console.log(data.results[0]);
+                controller.set('googleMapAddress', data.results[0]);
                 controller.set('lat',data.results[0].geometry.location.lat);
                 controller.set('lng',data.results[0].geometry.location.lng);
                 controller.set('zoom',15);
-                console.log(controller.get('lat'));
-                console.log(controller.get('lng'));
+                controller.set('locationSet',true);
             });
         },
+        // Add a trace point to the map
         addPoint: function(e){
             console.log(e);
             let location = e.latlng;
@@ -30,11 +35,32 @@ export default Ember.Controller.extend({
                 lng: location.lng
             });
         },
+        // User has moved around the map
         updateLocation(r, e) {
             let location = e.target.getLatLng();
             Ember.setProperties(r, {
                 lat: location.lat,
                 lng: location.lng
+            });
+        },
+        // Save a trace with all it's points and apply it to the business and address
+        submitTrace(){
+            let address = Ember.store.createRecord('address', {
+                company: this.get('company'),
+                address1: this.get(googleMapAddress.address_components[0].long_name) + ' ' + this.get(googleMapAddress.address_components[1].long_name),
+                address2: '',
+                city: this.get(googleMapAddress.address_components[3].long_name),
+                state: this.get(googleMapAddress.address_components[5].short_name),
+                zipcode: this.get(googleMapAddress.address_components[7].long_name),
+                longitude: this.get(googleMapAddress.geometry.location.lng),
+                latitude: this.get(googleMapAddress.geometry.location.lat),
+                addedDate: new Date()
+            });
+            let trace = Ember.store.createRecord('trace', {
+                company: this.get('company'),
+                address: address, 
+                createdDate: new Date(),
+                active: 1
             });
         }
     }
