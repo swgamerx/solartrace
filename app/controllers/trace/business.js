b33e3895897875a5376b8e3afaafc3daa211e603
swgamerx <santeau@gmail.com>
2017-06-18 17:02:45 -0500

    Transitions now work.
    Finally got transitions to work by moving the action to the controller
    for business route.

--- app/controllers/trace/business.js
+++ app/controllers/trace/business.js
@@ -6,15 +6,37 @@ export default Ember.Controller.extend({
     zoom: 10,
     selectedAddress: null,
     mapPoints: [],
-    // actions: {
-    //     selectLocation(model){
-    //         let address = this.get('store').query('address', {
-    //             orderBy: 'placeId',
-    //             equalto: model.placeId,
-    //             limitToLast: 10
-    //         }).then((addresses) => {
-    //             console.log(addresses);
-    //         });
-    //     }
-    // }
+    actions: {
+        selectLocation(address) {
+          this.get("store")
+            .query("address", {
+              orderBy: "placeId",
+              equalto: address.placeId,
+              limitToLast: 10
+            })
+            .then((addresses) => {
+              if (addresses.get("length") > 0) {
+                // If there is already a record
+              } else {
+                // there is no record found
+                var business = this.get("business");
+                let newAddress = this.get("store")
+                  .createRecord("address", {
+                    address1: address.address1,
+                    city: address.city,
+                    state: address.state,
+                    zipcode: address.zipcode,
+                    business: business,
+                    lat: address.lat,
+                    lng: address.lng,
+                    placeId: address.placeId,
+                    country: address.country
+                });
+                newAddress.save().then((address) => {
+                    this.transitionToRoute("trace.business.address", address);
+                 });
+              }
+            });
+        } // end selectLocation
+    }
 });
