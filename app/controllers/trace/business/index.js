3fc3ea11f7079a78a63c35b203c3b1de588f2928
swgamerx <santeau@gmail.com>
2017-06-20 22:51:17 -0500

    Adding the new address to business

--- app/controllers/trace/business/index.js
+++ app/controllers/trace/business/index.js
@@ -15,11 +15,13 @@ export default Ember.Controller.extend({
               limitToLast: 10
             })
             .then((addresses) => {
+                var business = this.get("model");
               if (addresses.get("length") > 0) {
                 // If there is already a record
+                business.get('addresses').pushObject(addresses[0]);
+                this.transitionToRoute("trace.business.address", addresses[0]);
               } else {
                 // there is no record found
-                var business = this.get("model");
                 let newAddress = this.get("store")
                   .createRecord("address", {
                     address1: address.address1,
@@ -33,7 +35,10 @@ export default Ember.Controller.extend({
                     country: address.country
                 });
                 newAddress.save().then((address) => {
-                    this.transitionToRoute("trace.business.address", address);
+                    business.get('addresses').pushObject(address);
+                    business.save().then(()=>{
+                        this.transitionToRoute("trace.business.address", address);
+                    });
                  });
               }
             });
