41d50caa20153a72e7839fb3fb5df76dced25f90
Sean Anteau <santeau@gmail.com>
2018-07-05 11:22:05 -0500

    Deleting old app
    deleting old files to restart project

--- app/models/business.js
+++
@@ -1,9 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-    name: DS.attr('string'), // Business's name
-    description: DS.attr('string'), // Description of the business
-    logo: DS.attr('string'), // business's logo
-    addresses: DS.hasMany('address'),  // all the addresses for this business
-    dateAdded: DS.attr('date') // Date business was added
-});
