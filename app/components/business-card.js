b91b27e1a02557cecf0a9bc3625db1ad7791342a
Sean Anteau <santeau@gmail.com>
2018-08-24 22:19:41 -0500

    Updating the index and the business card component
    feeding in more data to the business card to show addresses correctly and the number of traces

--- app/components/business-card.js
+++ app/components/business-card.js
@@ -1,8 +1,16 @@
 import Component from "@ember/component";
-import { get } from "@ember/object";
+import { get, computed } from "@ember/object";
 
 export default Component.extend({
-  actions: {
-
-  }
+  numberOfTraces: computed("model.addresses", function() {
+    let count = 0;
+    if (get(this, "model.addresses")) {
+      let addresses = get(this, "model.addresses");
+      for (let i = 0; i < addresses.length; i++) {
+        counted += addresses[i].length;
+      }
+    }
+    return count;
+  }),
+  actions: {}
 });
---
+++ app/routes/index.js
@@ -0,0 +1,10 @@
+import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
+
+export default Route.extend({
+    model(){
+        return RSVP.hash({
+            businesses: this.store.findAll('business')
+        });
+    }
+});
